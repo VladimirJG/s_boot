c0809374d793f7199ce915d02871b6836a5396ce
VladimirJG <vladimi85@gmail.com>
2023-10-26 16:56:32 +0300

    save to members & added redirect)

--- src/main/java/com/example/s_boot/members/controllers/MemberHandler.java
+++ src/main/java/com/example/s_boot/members/controllers/MemberHandler.java
@@ -1,16 +1,21 @@
 package com.example.s_boot.members.controllers;
 
+import com.example.s_boot.members.models.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/members")
 public class MemberHandler {
+    private static List<Member> members = new ArrayList<>();
+
     @GetMapping("")
     public String toMembers(Model model) {
-        model.addAttribute("firstName", "Jar");
-        model.addAttribute("lastName", "Trinity");
+        model.addAttribute("members", members);
         return "members/members";
     }
 
@@ -21,10 +26,9 @@ public class MemberHandler {
 
     @PostMapping("new_member/create")
     public String createMember(@RequestParam String firstName, @RequestParam String lastName, Model model) {
-        System.out.printf("First name: %s; Last name: %s \n", firstName, lastName);
-        model.addAttribute("firstName", firstName);
-        model.addAttribute("lastName", lastName);
-        //return "redirect:/members";
-        return "members/members";
+        //  System.out.printf("First name: %s; Last name: %s \n", firstName, lastName);
+        members.add(new Member(firstName, lastName));
+        return "redirect:/members";
+//        return "members/members";
     }
 }
