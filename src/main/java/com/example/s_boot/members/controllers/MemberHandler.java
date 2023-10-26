650a189d9bcc4410e9bfdfa0022b7385aa0e4784
VladimirJG <vladimi85@gmail.com>
2023-10-26 15:17:10 +0300

    members.html & new_member.html added

--- src/main/java/com/example/s_boot/members/controllers/MemberHandler.java
+++ src/main/java/com/example/s_boot/members/controllers/MemberHandler.java
@@ -1,17 +1,14 @@
 package com.example.s_boot.members.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/members")
 public class MemberHandler {
-    @GetMapping
+    @GetMapping("")
     public String toMembers() {
-        return "memebers/memebers";
+        return "members/members";
     }
 
     @GetMapping("/new_member")
@@ -19,7 +16,7 @@ public class MemberHandler {
         return "members/new_member";
     }
 
-    @PatchMapping("new_member/create")
+    @PostMapping("new_member/create")
     public String createMember(@RequestParam String firstName, @RequestParam String lastName) {
         System.out.printf("First name: %s; Last name: %s \n", firstName, lastName);
         return "redirect:/members";
