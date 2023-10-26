b52faa23d1b6c5217baa6e3b83050e8817b785c0
VladimirJG <vladimi85@gmail.com>
2023-10-26 10:54:02 +0300

    Initial commit

---
+++ src/main/java/com/example/s_boot/intro/controllers/Intro.java
@@ -0,0 +1,15 @@
+package com.example.s_boot.intro.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Intro {
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public String intoGet() {
+        return "Hello SpringBoot!";
+    }
+}
